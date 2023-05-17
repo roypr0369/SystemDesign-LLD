5d7141edef9b22981d065e70fe96d25a803cc512
Priyanshu Roy <74240049+roypr0369@users.noreply.github.com>
2023-05-18 01:39:39 +0530

    Create Student.java

---
+++ BuilderDesignSystem/Student.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+public class Student {
+    int rollNumber;
+    int age;
+    String name;
+    String fatherName;
+    String motherName;
+    List<String> subjects;
+
+    public Student(StudentBuilder studentBuilder) {
+        this.rollNumber = studentBuilder.rollNumber;
+        this.age = studentBuilder.age;
+        this.name = studentBuilder.name;
+        this.fatherName = studentBuilder.fatherName;
+        this.motherName = studentBuilder.motherName;
+        this.subjects = studentBuilder.subjects;
+    }
+
+    public String toString(){
+        return ""+ " roll number: " + this.rollNumber +
+                " age: " + this.age +
+                " name: " + this.name +
+                " father name: " + this.fatherName +
+                " mother name: " + this.motherName +
+                " subjects: " + subjects.get(0) + "," + subjects.get(1) + "," + subjects.get(2) + subjects.get(3);
+
+    }
+}
